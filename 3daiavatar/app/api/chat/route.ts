12ab8aca59916e4b88a6ae26f53170168d53bf13
Konrad Gnat <konradmgnat@gmail.com>
2024-02-24 16:19:45 -0800

    api with function calling

--- 3daiavatar/app/api/chat/route.ts
+++ 3daiavatar/app/api/chat/route.ts
@@ -1,14 +1,45 @@
 import { NextResponse } from "next/server";
 import { connectToDatabase } from "@/lib/mongodb";
 
+const AVATAR_ID = "65da532ee83fbb558d1ed662";
+
 const FIREWORKS_API_URL =
   "https://api.fireworks.ai/inference/v1/chat/completions";
 const FIREWORKS_API_KEY = process.env.FIREWORKS_API_KEY; // Assuming you've stored your API key in .env.local
 
 export async function POST(request: Request) {
   const requestBody = await request.json();
-  console.log("requestBody ", requestBody);
+  console.log("/api/chat --- requestBody ", requestBody);
   const { message } = requestBody;
+
+  const apiUrl = "https://api.fireworks.ai/inference/v1/chat/completions";
+  const messages = [
+    {
+      role: "system",
+      content:
+        "You are a Steve Jobs, answer the question and use the provided tools.",
+    },
+    {
+      role: "user",
+      content: "What are Nike's net income in 2022?",
+    },
+  ];
+
+  const tools = [
+    {
+      type: "function",
+      function: {
+        name: "get_latest_apple_news",
+        description: "Get latest information about news on apple.",
+        parameters: {
+          type: "string",
+          description:
+            "What information do you want to find out from the web on apple news",
+        },
+      },
+    },
+  ];
+
   try {
     const requestBody = {
       model: "accounts/fireworks/models/llama-v2-7b-chat",
